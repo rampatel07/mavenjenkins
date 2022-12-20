47aceea13d1bf5520b2fd2c0459e7af75bdc756a
rampatel07 <71453078+rampatel07@users.noreply.github.com>
2022-12-20 16:53:02 +0530

    my first commit

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,28 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UITest 
+{
+
+	@Test
+	public void startBrowser()
+	{
+		
+			WebDriverManager.chromedriver().setup();
+			 WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+	}
+	
+	
+}
